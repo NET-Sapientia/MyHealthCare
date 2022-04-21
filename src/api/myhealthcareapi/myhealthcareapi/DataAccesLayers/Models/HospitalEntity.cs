84c7f6b9c34dd0c15a557d38109b9cdefbf7bf34
donathdaniel <57301890+donathdaniel@users.noreply.github.com>
2022-04-21 18:38:20 +0300

    Api initial

---
+++ src/api/myhealthcareapi/myhealthcareapi/DataAccesLayers/Models/HospitalEntity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myhealthcareapi.DataAccesLayers.Models
+{
+    public class HospitalEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Contact { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Address { get; set; }
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+        public float Longitude { get; set; }
+        public float Latitude { get; set; }
+
+        public virtual ICollection<DepartmentEntity> Department { get; set; }
+        
+    }
+}
