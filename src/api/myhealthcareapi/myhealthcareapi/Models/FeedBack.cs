8a4860680f6165421fb7d7d2074dc67ccb589f88
donathdaniel <57301890+donathdaniel@users.noreply.github.com>
2022-05-04 19:02:33 +0300

    Feedback endpoints

---
+++ src/api/myhealthcareapi/myhealthcareapi/Models/FeedBack.cs
@@ -0,0 +1,16 @@
+﻿using myhealthcareapi.DataAccesLayers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myhealthcareapi.Models
+{
+    public class FeedBack
+    {
+        public int Id { get; set; }
+        public AppointmentEntity Appointment { get; set; }
+        public string Message { get; set; }
+        public float Billing { get; set; }
+    }
+}
---
+++ src/api/myhealthcareapi/myhealthcareapi/Services/IFeedbackService.cs
@@ -0,0 +1,15 @@
+﻿using myhealthcareapi.DataAccesLayers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myhealthcareapi.Services
+{
+    public interface IFeedbackService
+    {
+        public Task<List<FeedBackEntity>> GetAllClientFeedback(int clientId);
+
+        public Task<List<FeedBackEntity>> GetAllMedicFeedback(int medicId);
+    }
+}
