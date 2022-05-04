69be7ed4c8056ee5ce26d9f943a7ce7cd7c92815
Laca <bedolacika@yahoo.com>
2022-05-04 13:21:23 +0300

    Unit Tests in progress

---
+++ src/api/myhealthcareapi/ClientsControllerTest/ClientLoginTest.cs
@@ -0,0 +1,47 @@
+using Xunit;
+using myhealthcareapi.Models;
+using myhealthcareapi.Controllers;
+using myhealthcareapi.Services;
+using AutoMapper;
+using System;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClientsControllerTest
+{ 
+    public class ClientLoginTest
+    {
+
+        private LoginModel _testModel;
+        private ClientsController _controller;
+        private readonly Mock<IClientService> _clientRepoMock = new Mock<IClientService>();
+        private readonly Mock<IMapper> _mapperRepoMock = new Mock<IMapper>();
+
+        public ClientLoginTest()
+        {
+            _testModel = new LoginModel()
+            {
+                Email = "asdf@gmail.com",
+                Password = "asdfasdf"
+            };
+
+            _controller = new ClientsController(_clientRepoMock.Object, _mapperRepoMock.Object);
+        }
+
+        [Fact]
+        public async void ReturnNotFoundResult_whenClientDoesntExist()
+        {
+            _testModel.Email = "wrongemail@gmail.com";
+            var result = await _controller.Login(_testModel);
+            Assert.IsType<NotFoundObjectResult>(result);
+        }
+
+        [Fact]
+        public async void ReturnNotFoundResult_whenPasswordIsWrong()
+        {
+            _testModel.Password = "wrong";
+            var result = await _controller.Login(_testModel);
+            Assert.IsType<NotFoundObjectResult>(result);
+        }
+    }
+}
